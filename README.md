4f4bcaa8eef76731efa3c6d602f1f4cad34eeef4
points4host <points4host@gmail.com>
2020-08-09 20:48:34 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+convert Arabic numbers to English numbers
---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = (val) => {
+
+    var en = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+    ar  = ['١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩', '٠'];
+    for (var i = 0, num = en.length; i < num; i++) {
+        val = val.replace(new RegExp(ar[i], "g"), en[i]);
+    }
+
+    return val;
+}
