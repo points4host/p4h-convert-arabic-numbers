6914692003403574c99ea2f075fc5944239e33d8
points4host <60343931+points4host@users.noreply.github.com>
2020-08-09 23:33:51 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-module.exports = (val) => {
-
+function convert_to_English (val) {
     var en = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
-    ar  = ['١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩', '٠'];
+        ar  = ['١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩', '٠'];
     for (var i = 0, num = en.length; i < num; i++) {
-        val = val.replace(new RegExp(ar[i], "g"), en[i]);
+      val = val.replace(new RegExp(ar[i], "g"), en[i]);
     }
 
     return val;
 }
+module.exports.p4har = convert_to_English;
